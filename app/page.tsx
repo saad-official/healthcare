c89f245207ea02227b4b30b944689981858f1c92
saad-official <tommmyy890@gmail.com>
2024-07-06 19:53:32 +0500

    first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,39 @@
+import PatientForm from "@/components/forms/patient-form";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <>
-   <h1 className="font-bold text-white">Home</h1>
-  <Button>Click me</Button>
-  </>
+  return ( 
+    <div className="max-h-screen flex h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+      <Image  
+      src='/assets/icons/logo-full.svg'
+      height={1000}
+      width={1000}
+      alt="patient"
+      className="mb-12 h-10 w-fit"
+      />
+      <PatientForm />
+
+      <div className="text-14-regular mt-20 flex justify-between">
+      <p className="justify-items-end text-dark-600 xl:text-left">
+      @ 2024 Carepulse
+      </p>
+      <Link href='/?admin=true' className="text-green-500">
+      Admin
+      </Link>
+      </div>
+        </div>
+        </section> 
+
+        <Image className="max-w-[50%] side_img hidden md:block" src='/assets/images/onboarding-img.png'
+        height={1000}
+        width={1000}
+        alt="patient"
+   
+        /> 
+  </div>
   );
 }
