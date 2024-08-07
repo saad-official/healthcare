fda5367587c8b2cb256350556fdee48bce26b645
saad-official <tommmyy890@gmail.com>
2024-08-08 00:01:48 +0500

    started admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const AdminPage = () => {
+  return (
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+        <header className='admin-header'>
+            <Link href={'/'} className='cursor-pointer'>
+            
+            </Link>
+            </header>
+    </div>
+  )
+}
+
+export default AdminPage
