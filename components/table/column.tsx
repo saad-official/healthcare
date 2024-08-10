85da153ccfb452bc63f3583fb5b2c7a80ebc1ccb
saad-official <tommmyy890@gmail.com>
2024-08-10 21:25:11 +0500

    started to build data table

--- components/table/column.tsx
+++ components/table/column.tsx
@@ -12,14 +12,18 @@ import {
 import { Button } from "../ui/button";
 import { MoreHorizontal } from "lucide-react";
 import StatueBadge from "../status-badge";
+import { formatDateTime } from "@/lib/utils";
+import { Doctors } from "@/constant";
+import Image from "next/image";
+import AppointmentModal from "../AppointmentModal";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Payment = {
   id: string;
-  patient:{
-    name:string
-  }
+  patient: {
+    name: string;
+  };
   amount: number;
   status: Status;
   email: string;
@@ -27,27 +31,68 @@ export type Payment = {
 
 export const columns: ColumnDef<Payment>[] = [
   {
-    header:"ID",
-    cell:({row}) => <p className="text-14-medium">{row.index + 1}</p>
-
+    header: "ID",
+    cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p>,
   },
   {
-    accessorKey:"patient",
-    header:"Patient",
+    accessorKey: "patient",
+    header: "Patient",
 
-    cell:({row}) => {
-        const appointment = row.original;
-        return <p className="text-14-medium">{appointment?.patient?.name}</p>
-    }
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <p className="text-14-medium">{appointment?.patient?.name}</p>;
+    },
   },
-    {
+  {
     accessorKey: "status",
     header: "Status",
-    cell:({row}) => (
-        <div className="min-w-[115px]">
-            <StatueBadge status={row?.original?.status} />
-        </div> 
-    )
+    cell: ({ row }) => (
+      <div className="min-w-[115px]">
+        <StatueBadge status={row?.original?.status} />
+      </div>
+    ),
+  },
+  {
+    accessorKey: "schedule",
+    header: "Appointment",
+    cell: ({ row }) => (
+      <p className="text-14-regular min-w-[100px] ">
+        {formatDateTime(row?.original?.schedule).dateTime}
+      </p>
+    ),
+  },
+  {
+    accessorKey: "primaryPhysician",
+    header: () => "Doctor",
+    cell: ({ row }) => {
+      const doctor = Doctors.find(
+        (doc) => doc.name === row.original.primaryPhysician
+      );
+
+      return (
+        <div className="flex items-center gap-3">
+          <Image
+            src={doctor?.image}
+            alt={doctor?.name}
+            width={100}
+            height={100}
+            className="size-8"
+          />
+          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+        </div>
+      );
+    },
+  },
+  {
+    id:"actions",
+    header: () => <div className="pl-4"> Actions</div>,
+    cell:({row}) => {
+        return (
+            <div className="flex gap-1">
+                <AppointmentModal />
+            </div>
+        )
+    }
   },
   {
     accessorKey: "email",
@@ -94,5 +139,4 @@ export const columns: ColumnDef<Payment>[] = [
       );
     },
   },
-  
 ];
